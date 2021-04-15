d77f5e6f0f4d26532c3bc1a29fdc7bf8d39ff88b
Daban Sadiq <dabansadiq@icloud.com>
2021-04-15 15:32:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 
 console.log('%c HI', 'color: firebrick')
-let breedNames = [] 
+let breedNames = []
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
    document.addEventListener('DOMContentLoaded', function() {
-   
-   
+
+
     fetch(imgUrl)
     .then(response =>  response.json())
     .then((r)=>{
       displayImages(r)
-      
+
     });
 
 
@@ -21,8 +21,8 @@ const breedUrl = 'https://dog.ceo/api/breeds/list/all'
     then(response =>  response.json())
     .then((r)=>{
        displayBreeds(r)
-     
-      
+
+
     });
 
     });
@@ -33,42 +33,42 @@ const breedUrl = 'https://dog.ceo/api/breeds/list/all'
       const imges = document.querySelector('#dog-image-container')
 
         for (let i = 0; i < r.message.length; i++) {
-          
+
           const div =document.createElement('div');
           div.innerHTML = `<img src="${r.message[i]}" alt="" srcset="">`
           imges.append(div)
-        
-      }
-      
 
-      
+      }
+
+
+
 
 
     }
 
-    
+
     function displayBreeds(r){
       const ul = document.querySelector('#dog-breeds')
       breedNames = Object.keys(r.message)
-      
+
         for (let i = 0; i < breedNames.length; i++) {
-          
+
           const li =document.createElement('li');
           li.classList.add('test')
           li.innerText = breedNames[i]
           ul.append(li)
-        
+
       }
-     
-      
+
+
 
 
     }
 
 
     window.addEventListener('load', () => {
-      
-      
+
+
     const li =document.querySelectorAll('li')
     for (let i = 0; i < li.length; i++) {
       li[i].addEventListener("click", function() {
@@ -76,14 +76,14 @@ const breedUrl = 'https://dog.ceo/api/breeds/list/all'
       });}
 
       const select = document.querySelector('#breed-dropdown')
-     
+
       select.addEventListener('change',()=>{
         const ul = document.querySelector('#dog-breeds')
         ul.innerHTML=""
         for (let i = 0; i < breedNames.length; i++) {
-          
+
           if(breedNames[i].charAt(0) === select.value){
-            
+
             const li =document.createElement('li');
           li.classList.add('test')
           li.innerText = breedNames[i]
@@ -99,8 +99,8 @@ const breedUrl = 'https://dog.ceo/api/breeds/list/all'
         }
 
       })
-      
+
 
     });
-    
+
     
